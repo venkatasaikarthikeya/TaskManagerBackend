8f1416a7dd9aaadb59b0ee0446670f01122bb872
Venkata Sai Karthikeya <34571463+saikarthik2407@users.noreply.github.com>
2023-06-11 11:47:12 +0530

    TODO using in memory h2 database

---
+++ todo/src/main/java/com/alphalearn/todo/model/Todo.java
@@ -0,0 +1,50 @@
+package com.alphalearn.todo.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Author: Karthik
+ * Date: 6/11/2023
+ * Time: 11:17 AM
+ */
+
+@Entity
+@Table(name = "todo")
+@NamedQuery(name = "fetch_all_todos", query = "select t from Todo t")
+public class Todo {
+
+    @Id
+    @Column(name = "todo_id")
+    private Integer id;
+
+    @Column(name = "todo_subject")
+    private String subject;
+
+
+    public Todo() { }
+
+    public Todo(String subject) {
+        this.subject = subject;
+    }
+
+    public Todo(Integer id, String subject) {
+        this.id = id;
+        this.subject = subject;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
